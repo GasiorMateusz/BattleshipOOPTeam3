541728db8a4e6bf1c0a6fbcf7567a38ddd3abcad
gasiorMateusz <mateuszgasior314@gmail.com>
2022-10-26 16:46:59 +0200

    code cleaning

--- src/main/java/Square/Square.java
+++ src/main/java/Square/Square.java
@@ -2,8 +2,8 @@ package Square;
 
 public class Square {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
     SquareStatus status;
     public Square(int x,int y, SquareStatus status){
         this.x = x;
@@ -22,6 +22,7 @@ public class Square {
             status = SquareStatus.Hit;
         }
     }
+
     public SquareStatus getStatus(){
         return status;
     }
