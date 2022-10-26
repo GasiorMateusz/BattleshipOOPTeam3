37ff96b5052c9d57a063af1e3d2e6f1eeaf1b858
sandrac91 <sandra.b.ciura@gmail.com>
2022-10-26 10:54:33 +0200

    Move initiative methods to SeaBattle

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,55 +8,6 @@ public class Game {
     Player player1;
     Player player2;
 
-    /**
-     * check if enemy ship got hit and get
-     * @param enemy - opposite player
-     * @param playerShot - square chosen by the player
-     */
-    public void checkShot(Player enemy, Square playerShot) {
-        enemy.board.getSquare(playerShot).updateSquareStatus();
-
-    }
-
-    public void showMainMenu() {
-        boolean tryAgain = true;
-        int option;
-
-        while (tryAgain) {
-            display.menu();
-            option = input.getMenuOption();
-            switch (option) {
-                case 1:
-                    play();
-                    break;
-                case 2:
-                    // TODO: odczyt najlepszych wyników z pliku
-                    break;
-                case 3:
-                    tryAgain = false;
-                    break;
-            }
-        }
-    }
-
-    public void play() {
-        boardSetUp();
-        do {
-            if (playRound(player1)) break;
-            if (playRound(player2)) break;
-        } while (true);
-        display.gameOver();
-    }
-
-
-    public void boardSetUp() {
-        //TODO: in future, allow user choose beetween placement ships manually or randomly
-        // ustaw obie plansze na sztywno
-        BoardFactory boardFactory = new BoardFactory();
-        boardFactory.randomPlacement(player1.getBoard());
-        boardFactory.randomPlacement(player2.getBoard());
-
-    }
 
 
 
@@ -64,12 +15,13 @@ public class Game {
      * take all the actions required to make single player's move.
      *
      * @param player - player that is moving now
-     * @return true if enemy lost the game, otherwise false
+     * @return false if enemy lost the game, otherwise true
      */
 
     public boolean playRound(Player player) {
         Square playerShot = null;
         Player enemyPlayer;
+
         if (player == player1) {
             enemyPlayer = player2;
         } else {
@@ -84,9 +36,19 @@ public class Game {
         display.board();
         return enemyPlayer.isAlive();
 
+    }
 
+    /**
+     * check if enemy ship got hit and get
+     * @param enemy - opposite player
+     * @param playerShot - square chosen by the player
+     */
+
+    public void checkShot(Player enemy, Square playerShot) {
+        enemy.board.getSquare(playerShot).updateSquareStatus();
 
     }
+
 
     public boolean isGameOver() {
         return false;
@@ -97,7 +59,5 @@ public class Game {
     }
 
 }
-//glowna petla gry wyswietlajaca menu
-// liczenie strzalow
-// sprawdzanie highscore
-// ustawienie plansz na sztywno
+
+
--- src/main/java/SeaBattle.java
+++ src/main/java/SeaBattle.java
@@ -1,9 +1,54 @@
+import Board.BoardFactory;
+
 public class SeaBattle {
 
     Game game;
+    Input input;
+    Display display;
+    Player player1;
+    Player player2;
 
     public SeaBattle(Game game) {
         this.game = game;
+    }
+
+    public void showMainMenu() {
+        boolean tryAgain = true;
+        int option;
+
+        while (tryAgain) {
+            display.menu();
+            option = input.getMenuOption();
+            switch (option) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    // TODO: read high score from file
+                    break;
+                case 3:
+                    tryAgain = false;
+                    break;
+            }
+        }
+    }
+
+
+    public void play() {
+        boardSetUp();
+        do {
+            if (!game.playRound(player1)) break;
+            if (!game.playRound(player2)) break;
+        } while (true);
+        display.gameOver();
+    }
+
+    public void boardSetUp() {
+        //TODO: in future, allow user choose beetween placement ships manually or randomly
+        BoardFactory boardFactory = new BoardFactory();
+        boardFactory.randomPlacement(player1.getBoard());
+        boardFactory.randomPlacement(player2.getBoard());
+
     }
 
 }
