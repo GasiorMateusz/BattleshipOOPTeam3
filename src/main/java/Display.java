c8c38ac1806acee29ed7f63abb43e814b55596ca
sandrac91 <sandra.b.ciura@gmail.com>
2022-10-26 20:01:39 +0200

    Add possibility of hiding ships to Display.board()

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,23 +1,36 @@
 import Board.Board;
 import Square.Square;
+import Square.SquareStatus;
 
 public class Display {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001b[34m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_MAGENTA = "\u001B[35m";
 
     public void printMessage(String message) {
         System.out.println(message);
     }
 
-    public void printSquareStatus(Square shot) {
-        System.out.println(shot.getStatus() + "!\n");
+    public void printPlayer1Round() {
+        System.out.println(ANSI_CYAN + "\u001b[1m Player 1 " + ANSI_RESET + ANSI_BLUE + " >>>\n" + ANSI_RESET);
     }
 
+    public void printPlayer2Round() {
+        System.out.println(ANSI_CYAN + "\u001b[1m Player 2 " + ANSI_RESET + ANSI_BLUE + " >>>\n" + ANSI_RESET);
+    }
+
+    public void printSquareStatus(Square shot) {
+        System.out.println(ANSI_MAGENTA + shot.getStatus() + "!\n" + ANSI_RESET);
+    }
 
     public void printWrongMenuInputMessage() {
-        System.out.println("Wrong input, enter answer from 1 to 4!");
+        System.out.println(ANSI_RED + "Wrong input, enter answer from 1 to 4!" + ANSI_RESET);
     }
 
     public void printWrongShotInputMessage() {
-        System.out.println("Wrong input. Try again! Shot input example: A1!");
+        System.out.println(ANSI_RED + "Wrong input. Try again! Shot input example: A1!" + ANSI_RESET);
     }
 
     /**
@@ -33,7 +46,7 @@ public class Display {
         );
     }
 
-    public void shipPlacementOption(){
+    public void shipPlacementOption() {
         System.out.println(
                 "Choose ship placement mode:\n" +
                         "\t1. manual\n" +
@@ -42,30 +55,49 @@ public class Display {
     }
 
 
-
     /**
      * Prints a board instance.
+     *
      * @param ocean - instance to be printed
      */
 
-    public void board (Square[][] ocean) {
 
+    public void board(Square[][] ocean) {
+        boolean areShipsVisible = false;
         StringBuilder fields = new StringBuilder();
         for (int row = 0; row < ocean.length; row++) {
-                fields.append("\t").append(row + 1);
-            }
+            fields.append("\t").append(row + 1);
+        }
         fields.append("\n");
-        for (int row = 0; row < ocean.length; row++) {
-            fields.append((char) (row + 65));
-            for (int column = 0; column < ocean.length; column++) {
-                fields.append("\t").append(ocean[row][column].display());
+
+        if (areShipsVisible) {
+            for (int row = 0; row < ocean.length; row++) {
+                fields.append((char) (row + 65));
+                for (int column = 0; column < ocean.length; column++) {
+                    fields.append("\t").append(ocean[row][column].display());
+                }
+                fields.append("\n");
             }
-            fields.append("\n");
+        } else {
+            for (int row = 0; row < ocean.length; row++) {
+                fields.append((char) (row + 65));
+                for (int column = 0; column < ocean.length; column++) {
+                    if (ocean[row][column].getStatus().equals(SquareStatus.Missed) ||
+                            ocean[row][column].getStatus().equals(SquareStatus.Hit)) {
+                        fields.append("\t").append(ocean[row][column].display());
+                    } else {
+                        fields.append("\t").append(" ");
+                    }
+                }
+                fields.append("\n");
+            }
         }
         System.out.println(fields);
+
     }
 
-    public void highScores(){
+
+    public void highScores() {
 
     }
 
@@ -73,7 +105,7 @@ public class Display {
      * prints the outcome of the game when it is over.(Congratulations to the winner)
      */
     public void gameOver(int winner) {
-        System.out.printf("Congratulations! Player %o has won!%n",winner);
+        System.out.printf("Congratulations! Player %o has won!%n", winner);
     }
 
 }
