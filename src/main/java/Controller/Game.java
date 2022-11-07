96803fec0a616fdd7f394e1355d26fada9898d0e
gasiorMateusz <mateuszgasior314@gmail.com>
2022-11-07 16:38:00 +0100

    delete invalid documentation

--- src/main/java/Controller/Game.java
+++ src/main/java/Controller/Game.java
@@ -4,14 +4,6 @@ import Board.Point;
 import Player.Player;
 import Ship.Ship;
 public class Game {
-    /**
-     * Takes all the actions required to make single player's move.
-     *
-     * @param enemyPlayer - opposite player
-     * @param playerShot - array with shot's coordinates
-     *
-     * @return false if enemy lost the game, otherwise true
-     */
 
     public boolean playRound (Player enemyPlayer, Point point) {
 
