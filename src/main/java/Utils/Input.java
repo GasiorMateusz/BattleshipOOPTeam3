445dc66e0c4cdad059d17cb14264bffc017a030e
Dariusz Gaczyński <74984745+dariuszgaczynski@users.noreply.github.com>
2022-11-09 12:19:01 +0100

    Input class refactored

--- src/main/java/Utils/Input.java
+++ src/main/java/Utils/Input.java
@@ -10,16 +10,18 @@ import java.util.regex.Pattern;
 
 public class Input {
     private final Display display = new Display();
+    private final String MATCH_INPUT_FROM_1_TO_5 = "^[1-5]$";
+    private final String MATCH_INPUT_FROM_1_TO_2 = "^[1-2]$";
+    private final String MATCH_INPUT_COORDINATES_A_TO_J_AND_0_TO_9 = "^[a-j][0-9]{1,2}$";
     private final Scanner scanner = new Scanner(System.in);
-    boolean isShotInputCorrect = false;
-    boolean isDirectionInputCorrect = false;
-    boolean isPlacementSquareInputCorrect = false;
-    String squarePlayerShotInput = null;
-    String directionPlayerInput = null;
+
+    private String getUserInput() {
+        return scanner.nextLine();
+    }
 
     public int getMenuOption() {
-        String option = scanner.nextLine().strip();
-        Pattern pattern = Pattern.compile("^[1-5]$");
+        String option = getUserInput().strip();
+        Pattern pattern = Pattern.compile(MATCH_INPUT_FROM_1_TO_5);
         if (pattern.matcher(option).matches()) {
             return Integer.parseInt(option);
         } else {
@@ -27,22 +29,50 @@ public class Input {
         }
     }
 
-    public String getName() {
-        return scanner.nextLine().strip();
+    public String getPlayerNameFromUser() {
+        return getUserInput().strip();
     }
 
     public Point getShot() {
+        String squarePlayerShotInput;
+        boolean isShotInputNotCorrect;
+
         do {
-            isShotInputCorrect = isCorrect();
-            if (!isShotInputCorrect) {
+            squarePlayerShotInput = getUserInput();
+            isShotInputNotCorrect = validateIfShotInputIsNotCorrect(squarePlayerShotInput);
+            if (isShotInputNotCorrect) {
                 display.printWrongShotInputMessage();
             }
-        } while (!isShotInputCorrect);
-        return getChosenCoordinates();
+        } while (isShotInputNotCorrect);
+
+        return getChosenCoordinates(squarePlayerShotInput);
+    }
+
+    private boolean validateIfShotInputIsNotCorrect(String input) {
+        if (checkRegex(MATCH_INPUT_COORDINATES_A_TO_J_AND_0_TO_9, input)) {
+            return isCoordinateNotInRange(input);
+        }
+        return true;
+    }
+
+    private boolean checkRegex(String regexPattern, String input) {
+        Pattern pattern = Pattern.compile(regexPattern);
+        return pattern.matcher(input).matches();
+    }
+
+    private Point getChosenCoordinates(String input) {
+        char rowChar = input.charAt(0);
+        int row = rowChar - 97;
+        int column = Integer.parseInt(input.substring(1)) - 1;
+        return new Point(row, column);
+    }
+    private boolean isCoordinateNotInRange(String input) {
+        return (Integer.parseInt(input.substring(1)) - 1) > 9 || (Integer.parseInt(input.substring(1)) - 1) < 0;
     }
 
     public Point getPlacementSquare(Board board) {
         Point point;
+        boolean isPlacementSquareInputCorrect;
         do {
             point = getShot();
             isPlacementSquareInputCorrect = board.canNewShipBeHere(point);
@@ -55,17 +85,17 @@ public class Input {
 
     public int getPlacementOption() {
         String option;
-        Pattern pattern = Pattern.compile("^[1-2]$");
         do {
-            option = scanner.nextLine().strip();
-        }
-        while (!pattern.matcher(option).matches());
+            option = getUserInput().strip();
+        } while (!checkRegex(MATCH_INPUT_FROM_1_TO_2, option));
         return Integer.parseInt(option);
     }
 
     public Direction getDirection() {
+        String directionPlayerInput = getUserInput().strip().toUpperCase();
+        boolean isDirectionInputCorrect;
         do {
-            isDirectionInputCorrect = isDirectionCorrect();
+            isDirectionInputCorrect = isDirectionCorrect(directionPlayerInput);
             if (!isDirectionInputCorrect) {
                 display.printWrongDirectionInputMessage();
             }
@@ -73,32 +103,7 @@ public class Input {
         return Direction.valueOf(directionPlayerInput);
     }
 
-    public boolean isDirectionCorrect() {
-        directionPlayerInput = scanner.nextLine();
-        return Arrays.stream(Direction.values()).anyMatch(direction -> directionPlayerInput.equals(direction.name()));
-    }
-
-    public boolean isCorrect() {
-        if (!checkRegex()) {
-            return false;
-        }
-        return !isCoordinateNumberOk();
-    }
-
-    private Point getChosenCoordinates() {
-        char rowChar = squarePlayerShotInput.charAt(0);
-        int row = rowChar - 97;
-        int column = Integer.parseInt(squarePlayerShotInput.substring(1)) - 1;
-        return new Point(row, column);
-    }
-
-    private boolean isCoordinateNumberOk() {
-        return (Integer.parseInt(squarePlayerShotInput.substring(1)) - 1) > 9 || (Integer.parseInt(squarePlayerShotInput.substring(1)) - 1) < 0;
-    }
-
-    private boolean checkRegex() {
-        squarePlayerShotInput = scanner.nextLine();
-        Pattern pattern = Pattern.compile("^[a-j][0-9]{1,2}$");
-        return pattern.matcher(squarePlayerShotInput).matches();
+    public boolean isDirectionCorrect(String input) {
+        return Arrays.stream(Direction.values()).anyMatch(direction -> input.equals(direction.name()));
     }
 }
